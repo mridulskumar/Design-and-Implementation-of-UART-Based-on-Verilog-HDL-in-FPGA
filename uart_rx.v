246c3e2cd0135bdd59e845582d463f125ff58a74
Mridul S Kumar <149874226+mridulskumar@users.noreply.github.com>
2025-11-02 11:51:34 +0530

    uart_rx.v

--- uart_rx.v
+++ uart_rx.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.10.2025 18:01:49
-// Design Name: 
-// Module Name: uart_rx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module uart_rx (
     input wire i_clk,
     input wire i_rst_n,
